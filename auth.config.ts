499515b8e0eb0cbaabacfd31b546e79930578ead
djhi12 <ido20002@byui.edu>
2024-02-20 20:35:44 +0800

    updated

--- auth.config.ts
+++ auth.config.ts
@@ -12,10 +12,10 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/', nextUrl));
       }
       return true;
     },
   },
   providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+} satisfies NextAuthConfig; 
